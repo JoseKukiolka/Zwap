16adb7becccf4293afa2afe627299b9aa41d4658
KUKIOLKA JOSEFINA <48459624@ort>
2025-05-30 14:28:30 -0300

    PRIMER COMIIITTTT

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const app = express ();
+const dotenv = require ("dotenv").config();
+const cors = require ("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, console.log("Server Running"))
+
+https://www.youtube.com/watch?v=XKwOsn37KCc
